2b8092e8a09c38b10813be07b5ece2fe5956b228
osharghi <osharghi@gmail.com>
2019-01-25 15:20:53 -0800

    Included problem link to GroupAnagram

--- Algorithms/src/GroupAnagrams.java
+++ Algorithms/src/GroupAnagrams.java
@@ -3,22 +3,24 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+//https://leetcode.com/problems/group-anagrams/
+
 public class GroupAnagrams {
 
 	public List<List<String>> groupAnagrams(String[] strs) {
-	     
+
         List<List<String>> results = new ArrayList<>();
-        
+
         if(strs.length == 0) return results;
-        
+
         HashMap<String, List<String>> map = new HashMap<>();
-        
+
         for(String s: strs)
         {
             char[] arr = s.toCharArray();
             Arrays.sort(arr);
             String sSort = new String(arr);
-            
+
             if(map.containsKey(sSort))
             {
                 map.get(sSort).add(s);
@@ -29,18 +31,18 @@ public class GroupAnagrams {
                 list.add(s);
                 map.put(sSort, list);
             }
-            
+
         }
-        
+
         for(String key: map.keySet())
         {
             List<String> list = map.get(key);
             results.add(list);
         }
-        
+
         return results;
-        
+
     }
-	
-	
+
+
 }
