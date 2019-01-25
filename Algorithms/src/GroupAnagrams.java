c476837b6631eeea66a02be488465a96e3d28bcc
osharghi <osharghi@gmail.com>
2019-01-25 15:19:01 -0800

    Added new LeetCode problem

---
+++ Algorithms/src/GroupAnagrams.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+
+	public List<List<String>> groupAnagrams(String[] strs) {
+	     
+        List<List<String>> results = new ArrayList<>();
+        
+        if(strs.length == 0) return results;
+        
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for(String s: strs)
+        {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String sSort = new String(arr);
+            
+            if(map.containsKey(sSort))
+            {
+                map.get(sSort).add(s);
+            }
+            else
+            {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(sSort, list);
+            }
+            
+        }
+        
+        for(String key: map.keySet())
+        {
+            List<String> list = map.get(key);
+            results.add(list);
+        }
+        
+        return results;
+        
+    }
+	
+	
+}
