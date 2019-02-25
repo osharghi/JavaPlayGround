53bf774e853bf12339811ef84aa6302927980548
osharghi <osharghi@gmail.com>
2019-02-25 09:33:54 -0800

    LeetCode: 70. Climbing Stairs

---
+++ Algorithms/src/ClimbingStairs.java
@@ -0,0 +1,23 @@
+
+//https://leetcode.com/problems/climbing-stairs/
+
+public class ClimbingStairs {
+	
+	public int climbStairs(int n) {
+        
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[dp.length-1];
+    }
+
+}
